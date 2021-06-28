6f8ce93686b9e506d4fbcf2625d9aee27f8eda24
Damla Beyaz <ddurmaz88@gmail.com>
2021-06-28 20:47:03 +0200

    Adds solutions for attribute binding

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,8 @@
+Vue.createApp({
+    data: function() {
+        return {
+            courseGoal: "I want to finish Vue.js course until the end of this week.",
+            vueLink: "https://vuejs.org"
+        };
+    }
+}).mount("#user-goal");
