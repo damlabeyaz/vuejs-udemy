28ee1b10df64f83749405786bfacad5138cd97bf
Damla Beyaz <ddurmaz88@gmail.com>
2021-06-30 17:43:57 +0200

    Adds code for first section

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,8 +1,20 @@
 Vue.createApp({
     data: function() {
         return {
-            courseGoal: "I want to finish Vue.js course until the end of this week.",
+            courseGoalA: "I want to finish Vue.js course until the end of this week.",
+            courseGoalB: "I want to master Vue.js course until the end of this week.",
+            courseGoalC: "Learn <i>HTML<i/>",
             vueLink: "https://vuejs.org"
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 }).mount("#user-goal");
